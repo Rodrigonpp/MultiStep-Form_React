3198e008d3eb0ff2301aa35bd835ce6052e57e30
Rodrigo  Nunes <149826690+Rodrigonpp@users.noreply.github.com>
2026-02-12 13:06:59 -0300

    Add files via upload

---
+++ src/components/Thanks.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
+import "./Thanks.css";
+
+const emojiData = {
+  unsatisfied: <BsFillEmojiFrownFill />,
+  neutral: <BsFillEmojiNeutralFill />,
+  satisfied: <BsFillEmojiSmileFill />,
+  very_satisfaied: <BsFillEmojiHeartEyesFill />,
+};
+
+const Thanks = ({ data }) => {
+  return (
+    <div className="thanks-container">
+      <h2>Falta pouco...</h2>
+      <p>
+        Sua opnião é muito importante, em breve você receberá um cupom de 10% de
+        desconto para a sua próxima compra.
+      </p>
+      <p>Para concluir sua avaliação clique nobotão de Enviar abaixo.</p>
+      <h3>Aqui está o resumo da sua avaliação, {data.name}:</h3>
+      <p className="review-data">
+        <span>Satisfação com o produto: {emojiData[data.review]}</span>
+      </p>
+      <p className="review-data">
+        <span>Comentário: {data.comment}</span>
+      </p>
+    </div>
+  );
+};
+
+export default Thanks;
